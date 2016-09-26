a4e562a3ec7ce4dd84f07632de897d1ebe6ee83b
PavoReal <Garrison.peacock@gmail.com>
2016-09-25 20:02:16 -0700

    Fixed Quicksort so it actually works now

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,73 @@
 #include <array>
+#include <random>
+#include <algorithm>
 #include <SDL2/SDL.h>
-
-#define rand() (arc4random() % ((unsigned)RAND_MAX + 1))
 
 using uint = unsigned int;
 using uint8 = uint8_t;
 using uint32 = uint32_t;
 
-struct element;
-void Render(const element* e);
-
 SDL_Window* window;
 SDL_Renderer* renderer;
 
-struct element
+template <typename T> constexpr inline
+T Map(T x, T minIn, T maxIn, T minOut, T maxOut)
 {
-    uint32 value;
-    uint8 r, g, b;
-
-    inline
-    element() : value(0), r(0), g(0), b(0) {}
-
-
-    friend inline
-    bool operator > (const element& a, const element& b)
-    {
-        bool result;
-
-        result = a.value > b.value;
-
-        Render(&a);
-
-        return result;
-    }
-
-    friend inline
-    bool operator < (const element& a, const element& b)
-    {
-        bool result;
-
-        result = a.value < b.value;
-
-        Render(&a);
-
-        return result;
-    }
-};
-
-void Render(element* e, int count)
-{
-    constexpr auto ELEMENT_HEIGHT = 400;
-    constexpr auto ELEMENT_WIDTH = 800 / 100;
-
-    SDL_Rect rects[count];
-    for (auto i = 0; i < count; ++i)
-    {
-        rects[i].h = ELEMENT_HEIGHT;
-        rects[i].w = ELEMENT_WIDTH;
-        rects[i].x = ;
-        rects[i].y = ;
-    }
-
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-    SDL_RenderDrawRects(renderer, rects, count);
-    SDL_RenderPresent(renderer);
-
-    SDL_Delay(250);
+    return (x - minIn) * (maxOut - minOut) / (maxIn - minIn) + minOut;
 }
 
-void QuickSort(element* elements, int elementsCount, int left, int right)
+void Render(uint32* elements, int count)
+{
+    constexpr auto ELEMENT_WIDTH = (800 / 100) - 1;
+
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+    for (auto i = 0; i < count; ++i)
+    {
+        SDL_Rect rect = {};
+        rect.x = i * ELEMENT_WIDTH;
+        rect.y = 0;
+        rect.w = ELEMENT_WIDTH;
+        rect.h = Map(elements[i], 0U, 100U, 100U, 500U);
+
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(50);
+}
+
+void Update()
+{
+    SDL_Event e;
+    while (SDL_PollEvent(&e))
+    {
+        if (e.type == SDL_QUIT)
+        {
+            exit(0);
+        }
+    }
+}
+
+void QuickSort(uint32* elements, int elementsCount, int left, int right)
 {
     auto i = left;
     auto j = right;
-    auto pivot = elements[(left + right) / 2].value;
-    element tmp;
+    auto pivot = elements[(left + right) / 2];
+    uint32 tmp;
 
     Render(elements, elementsCount);
 
     /* partition */
     while (i <= j)
     {
-        while (elements[i].value < pivot)
+        while (elements[i] < pivot)
         {
             i++;
         }
 
-        while (elements[j].value > pivot)
+        while (elements[j] > pivot)
         {
             j--;
         }
@@ -99,13 +81,20 @@ void QuickSort(element* elements, int elementsCount, int left, int right)
             i++;
             j--;
         }
+
+        Render(elements, elementsCount);
+        Update();
     };
 
-    /* recursion */
     if (left < j)
+    {
         QuickSort(elements, elementsCount, left, j);
+    }
+
     if (i < right)
+    {
         QuickSort(elements, elementsCount, i, right);
+    }
 }
 
 int main()
@@ -115,23 +104,17 @@ int main()
     window = SDL_CreateWindow("Visual Sort", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    std::array<element, 100> elements = {};
+    std::array<uint32, 100> elements = {};
 
-    int id = 0;
-    for (auto& i : elements)
-    {
-        i = {};
-        i.r = static_cast<uint8>(rand() % 255);
-        i.g = static_cast<uint8>(rand() % 255);
-        i.b = static_cast<uint8>(rand() % 255);
-        i.value = arc4random();
-
-        id += 2;
-    }
-
-    SDL_RenderClear(renderer);
+    std::srand(static_cast<unsigned>(std::time(0)));
+    std::generate(elements.begin(), elements.end(), []() { return std::rand() % 100; });
 
     QuickSort(elements.__elems_, static_cast<int>(elements.size()), 0, static_cast<int>(elements.size()));
+
+    for (;;)
+    {
+        Update();
+    }
 
     return 0;
 }
